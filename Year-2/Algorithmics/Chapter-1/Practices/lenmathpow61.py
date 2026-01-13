fd2641784219d420b2ddff7eb0fa6ca5cc8f4522
AnaGM2 <anagilmolina2@gmail.com>
2026-01-13 19:20:03 +0100

    Delete Year-2/Algorithmics/Chapter-1/Practices/lenmathpow61.py

--- Year-2/Algorithmics/Chapter-1/Practices/lenmathpow61.py
+++
@@ -1,9 +0,0 @@
-import math
-cadena1 = input("Introduce una cadena: ")
-cadena2 = input("Introduce otra cadena: ")
-l1 = len(cadena1)
-l2 = len(cadena2)
-print(math.pow(l1, l2))
-
-# len da la longitud de la cadena.
-# math.pow calcula l1 (longitud de cadena1) elevado a l2 (longitud de cadena2).
