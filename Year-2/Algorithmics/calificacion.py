f4a13699029f7de56b33b8f8f3c984ce0f87fce4
AnaGM2 <anagilmolina2@gmail.com>
2026-01-13 19:24:11 +0100

    Add files via upload

---
+++ Year-2/Algorithmics/calificacion.py
@@ -0,0 +1,12 @@
+nota = float(input('Introduce la nota: '))
+
+if nota < 5:
+    print('Suspenso')
+elif nota < 7:
+    print('Aprobado')
+elif nota < 9:
+    print('Notable')
+elif nota < 10:
+    print('Sobresaliente')
+else:
+    print('Matrícula de honor')
