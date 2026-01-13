a22c4d8bbff040424bcb63e2b7c32653ed09afa4
AnaGM2 <anagilmolina2@gmail.com>
2026-01-13 19:19:52 +0100

    Delete Year-2/Algorithmics/Chapter-1/Practices/errorfactorial63.py

--- Year-2/Algorithmics/Chapter-1/Practices/errorfactorial63.py
+++
@@ -1,9 +0,0 @@
-num = int(input('Introduce un entero: '))
-
-fact = 0
-for i in range(num, 2, -1):
-    fact = fact * i
-
-print('El factorial de ', num, ' es ', fact)
-
-# El error es que inicialmente fact=0, por lo que al multiplicarlo por cada i sigue dando 0.
