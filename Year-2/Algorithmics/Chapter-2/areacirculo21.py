7714f80266287b16638c76b33b4239d42702663a
AnaGM2 <anagilmolina2@gmail.com>
2026-01-13 19:25:31 +0100

    Add files via upload

---
+++ Year-2/Algorithmics/Chapter-2/areacirculo21.py
@@ -0,0 +1,8 @@
+# Programa que calcula el área de un círculo a partir de su radio.
+
+from math import pi
+
+radio = float(input("Introduce el radio: "))
+area = pi * radio ** 2
+
+print('El área es', area)
