72d24b70a256cd3c3330bdba79e13fcbc561a79a
AnaGM2 <anagilmolina2@gmail.com>
2026-01-13 19:19:44 +0100

    Delete Year-2/Algorithmics/Chapter-1/Practices/depuracion5.py

--- Year-2/Algorithmics/Chapter-1/Practices/depuracion5.py
+++
@@ -1,9 +0,0 @@
-num1 = int(input("Dame el primer sumando: "))
-num2 = int(input("Dame el segundo sumando: "))
-
-suma = num1+num2
-
-if suma == 10:
-    print("La suma es 10")
-else:
-    print("La suma es distinta de 10")
